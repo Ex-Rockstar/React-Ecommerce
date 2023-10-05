63e32c68af42f4ac3278e11e60471cbcf59b1ad8
Ruthika <ruthirockstar2218@gmail.com>
2023-10-06 00:00:50 +0530

    Added Home page layout

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
+export const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products, loading, error }}>
+      {children}
+    </ProductContext.Provider>
+  );
 };
 
-export default ProductContext;
+export default ProductProvider;
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
 import React from 'react';
-
+import { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext';
+import Product from '../components/Product';
 const Home = () => {
-  return <div>Homepage</div>;
+  const {products}= useContext(ProductContext);
+  console.log(products)
+  const filteredProducts = products.filter(item=>{
+    return (item.category === "electronics"  ||item.category === "jewelery")
+  })
+  // console.log(filteredProducts);
+
+
+
+  return <div>
+    <section className='py-6'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-5 gap-[30px] max-w-screen-sm mx-auto md:max-w-none md:mx-0 '>
+          {filteredProducts.map(product => {
+            return <div className='w-full h-[300px] bg-teal-100' key={product.id}>
+              {/* {product.title}
+               */}
+               <Product key={product.id}/>
+            </div>  
+          })}
+        </div>
+      </div>
+    </section>
+  </div>;
 };
 
 export default Home;
