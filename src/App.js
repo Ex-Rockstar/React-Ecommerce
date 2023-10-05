259e12f5b9b3df4c8dc6fd7102dd51f53fea3019
Ruthika <ruthirockstar2218@gmail.com>
2023-10-05 22:03:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Home from'./pages/Home';
+import ProductDetails from './pages/ProductDetails'
+
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return <div className='overflow-hidden'>
+    <Router>
+      <Routes>
+        <Route path='/' element={Home}/>
+        <Route path='/product/:id' element={ProductDetails}/>
+        <Sidebar/>
+        
+      </Routes>
+    </Router>
+  </div>;
+};
+
+export default App;
